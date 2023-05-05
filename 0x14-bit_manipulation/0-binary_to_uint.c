4c0bfbafee573d9b323cb5e16fff0418844729ff
YanickBJ <zottoricardo@gmail.com>
2023-05-05 12:13:15 -0700

    binaire

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,22 +16,12 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for (x = 0; b[x] != '\0'; x++)
+	while (b[x] != '\0')
 	{
-		if (b[x] == '0')
-		{
-			num <<= 1;
-		}
-		else
-			if (b[x] == '1')
-			{
-				num <<= 1;
-				num |= 1;
-			}
-			else
-			{
+		if (b[x] < '0' || b[x] > '1')
 			return (0);
-			}
+		num = num * 2 + (b[x] - '0');
+		x++;
 	}
-return (num);
+	return (num);
 }
