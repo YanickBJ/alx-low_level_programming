fd6c6a553b3dbe099beeb6747474fa1b8ada7c45
YanickBJ <zottoricardo@gmail.com>
2023-05-09 01:27:59 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fod < 0)
 		return (-1);
 
-	if (text_content == 0)
+	if (text_content != 0)
 	{
 		while (text_content[b])
 			b++;
